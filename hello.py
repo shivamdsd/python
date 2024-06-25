70a58b4d0892c74490de3ac9b178428c391c9f1a
shubham-dsdlawfirm <147094096+shubham-dsdlawfirm@users.noreply.github.com>
2024-06-25 06:46:32 -0400

    added to files first is hello.py and second is sample.txt

---
+++ hello.py
@@ -0,0 +1,27 @@
+items = ['apple', 'banans', 'cherry', 'apricot', 'blueberry', 'coconut', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon', 'mango', 'nectarine', 'orange', 'papaya', 'quince', 'raspberry', 'strawberry', 'tangerine', 'watermelon', 'zucchini']
+
+# First step is understanding the problem
+
+
+# second step is to formulate an algorithm
+# 1. Iterate through all the items in the list 
+# 2. Check if the item starts with the letter 'a'
+# 3. Then we have to check is there is key with the first letter of the item in the dictionary
+# 4. if the key exists, then we have to append the item to the list of values of the key
+# 5. if the key does not exist, then we have to create a new key with the first letter of the item and assign the item to the key
+
+
+# Third step is dry run the algorithm with an example
+
+# Fourth step is to implement the algorithm means write the code
+
+def group_items(items):
+    result = {}
+    for item in items:
+        key = item[0]
+        if key not in result:
+            result[key] = []
+        result[key].append(item)
+    return result
+
+print(group_items(items))
